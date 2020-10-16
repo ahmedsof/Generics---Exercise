24c02889a74ca631091467e4aefc8638e230bdac
Ahmed Softev <ahmed_v@abv.bg>
2020-10-17 00:03:40 +0300

    C#

---
+++ Generic Count Method String/Box.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodString
+{
+    public class Box<T> where T: IComparable
+    {
+
+        public List<T> Values { get; set; } = new List<T>();
+
+        public Box(List<T> values)
+        {
+            this.Values = values;
+        }
+
+        public void Swap(int first, int second)
+        {
+            var tempValue  = this.Values[first];
+            this.Values[first] = this.Values[second];
+            this.Values[second] = tempValue;
+        }
+
+        public int GetCountOfGreaterValues(T value)
+        {
+
+            int counter = 0;
+
+            foreach (T currentValue in this.Values)
+            {
+                if (value.CompareTo(currentValue) < 0)
+                {
+                    counter++;
+                }
+                
+            }
+            return counter;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (T value in this.Values)
+            {
+                sb.AppendLine($"{value.GetType()}: {value}");
+            }
+            return sb.ToString().Trim();
+        }
+    }   
+}
---
+++ Generic Count Method String/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            List<string> values = new List<string>();
+
+            for (int i = 0; i < input; i++)
+            {
+                string currentValue = Console.ReadLine();
+                values.Add(currentValue);
+            }
+
+            string valueToCompare = Console.ReadLine();
+
+            Box<string> box = new Box<string>(values);
+
+            int greatValue = box.GetCountOfGreaterValues(valueToCompare);
+            Console.WriteLine(greatValue);
+        }
+    }
+}
