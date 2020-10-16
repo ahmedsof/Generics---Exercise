bc92fa335a5672fd05f29b014f88c7512c791399
Ahmed Softev <ahmed_v@abv.bg>
2020-10-16 22:58:55 +0300

    C#

---
+++ Generic Box of Integer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericBoxofString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                Box<int> box = new Box<int>(input);
+
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
