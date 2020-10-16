a30716a49ce9d8c5b04da67a3df86a234d5baed3
Ahmed Softev <ahmed_v@abv.bg>
2020-10-16 23:47:22 +0300

    C#

---
+++ Generic Swap Method Integer/Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodString
+{
+    public class Box<T>
+    {
+
+        public List<T> Values { get; set; } = new List<T>();
+
+        public Box(List<T> values)
+        {
+            this.Values = values;
+        }
+
+        public void Swap(int first, int second)
+        {
+            var tempValue  = this.Values[first];
+            this.Values[first] = this.Values[second];
+            this.Values[second] = tempValue;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (T value in this.Values)
+            {
+                sb.AppendLine($"{value.GetType()}: {value}");
+            }
+            return sb.ToString().Trim();
+        }
+    }   
+}
