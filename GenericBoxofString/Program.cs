0991f01298f8cbb02951f8419e70867c1684171b
Ahmed Softev <ahmed_v@abv.bg>
2020-10-16 22:52:44 +0300

    C#

---
+++ GenericBoxofString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericBoxofString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                string input = Console.ReadLine();
+
+                Box<string> box = new Box<string>(input);
+
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
