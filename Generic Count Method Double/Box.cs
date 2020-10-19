71789cd103433315019b4cdb65be87de74e6eb1e
Ahmed Softev <ahmed_v@abv.bg>
2020-10-19 19:33:14 +0300

    C#

---
+++ Generic Count Method Double/Box.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodString
+{
+    public class Box<T> where T: IComparable
+    {
+
+        public List<T> Values { get; set; } = new List<T>();
+
+        public Box(List<T> values)
+        {
+            this.Values = values;
+        }
+
+        //public void Swap(double first, double second)
+        //{
+        //    double tempValue = Values[first];
+        //    Values[first] = Values[second];
+        //    Values[second] = tempValue;
+        //}
+
+        public int GetCountOfGreaterValues(T value)
+        {
+
+            int counter = 0;
+
+            foreach (T currentValue in this.Values)
+            {
+                if (value.CompareTo(currentValue) < 0)
+                {
+                    counter++;
+                }
+                
+            }
+            return counter;
+        }
+        //public override string ToString()
+        //{
+        //    StringBuilder sb = new StringBuilder();
+        //    foreach (T value in this.Values)
+        //    {
+        //        sb.AppendLine($"{value.GetType()}: {value}");
+        //    }
+        //    return sb.ToString().Trim();
+        //}
+    }   
+}
---
+++ Generic Count Method Double/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            List<double> values = new List<double>();
+
+            for (int i = 0; i < input; i++)
+            {
+                double currentValue = double.Parse(Console.ReadLine());  
+                values.Add(currentValue);
+            }
+
+            double valueToCompare = double.Parse(Console.ReadLine());
+
+            Box<double> box = new Box<double>(values);
+
+            double greatValue = box.GetCountOfGreaterValues(valueToCompare);
+            Console.WriteLine(greatValue);
+        }
+    }
+}
